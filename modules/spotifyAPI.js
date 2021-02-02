4ab72739843eb650fe05aae65756bd27b76f1f66
benl95 <beno_1995@hotmail.com>
2021-02-02 21:20:12 +0100

    feat: retrieve data from api

--- modules/spotifyAPI.js
+++ modules/spotifyAPI.js
@@ -16,15 +16,36 @@ const hash = window.location.hash
 window.location.hash = '';
 
 // Set token
-let _token = hash.access_token;
+let access_token = hash.access_token;
 const authEndpoint = 'https://accounts.spotify.com/authorize';
 const scopes = ['user-read-email', 'user-read-private'];
 
+const options = {
+	method: 'get',
+	url: 'https://accounts.spotify.com/api/token',
+	headers: {
+		Authorization: 'Bearer ' + access_token,
+		'Content-Type': 'application/x-www-form-urlencoded',
+	},
+	json: true,
+};
+
 // If there is no token, redirect to Spotify authorization
 connectToSpotify.addEventListener('click', () => {
-	if (!_token) {
+	if (!access_token) {
 		window.location = `${authEndpoint}?client_id=${clientID}&redirect_uri=${redirectURI}&scope=${scopes.join(
 			'%20'
 		)}&response_type=token`;
 	}
 });
+
+if (access_token) {
+	fetch('https://api.spotify.com/v1/me', options)
+		.then((response) => {
+			console.log('Response: ', response);
+			return response.json();
+		})
+		.then((data) => {
+			console.log('Data: ', data);
+		});
+}
