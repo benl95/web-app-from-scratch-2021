84da6268db423278b5c26e5150b1d575d93aeb5c
benl95 <beno_1995@hotmail.com>
2021-02-05 09:50:17 +0100

    Change redirectURI

--- authorization.js
+++ authorization.js
@@ -1,3 +1,3 @@
 export const clientID = '5470641b38f34b889f84c88912b013a0';
 export const clientSecret = '9e364847d4ca45c6b782ca346de3c51b';
-export const redirectURI = 'http://127.0.0.1:5500/index.html';
+export const redirectURI = 'https://spotify-authentication.netlify.app';
