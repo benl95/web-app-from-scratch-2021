24405050208de835ccf98619b4fd7cd585721252
benl95 <beno_1995@hotmail.com>
2021-02-08 15:28:00 +0100

    Rename authorization to config

--- authorization.js
+++
@@ -1,3 +0,0 @@
-export const clientID = '5470641b38f34b889f84c88912b013a0';
-export const clientSecret = '9e364847d4ca45c6b782ca346de3c51b';
-export const redirectURI = 'https://spotify-authentication.netlify.app';
