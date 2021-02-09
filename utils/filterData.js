3151caf38a5edc587da6a487294f2b2d67cf7834
benl95 <beno_1995@hotmail.com>
2021-02-09 17:16:42 +0100

    update: refactor function to filter track name

--- utils/filterData.js
+++ utils/filterData.js
@@ -1,4 +1,4 @@
-// Filter data based on column
+// Filter data based on column, return the value of a track name and create a new array
 export const filterData = (array, column) => {
-	return array.map((data) => data[column]);
+	return array.map((data) => data[column].name);
 };
