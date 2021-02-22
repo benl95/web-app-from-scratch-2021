815fc514e419427f8d16b95f3296918a65a355a1
benl95 <beno_1995@hotmail.com>
2021-02-22 14:12:49 +0100

    feat: render retrieved data to view

--- render/homeView.js
+++ render/homeView.js
@@ -1,1 +1,25 @@
+import { handleData } from '../data/handleData.js';
 
+const DOMElements = {
+	itemContainer: 'item-container',
+};
+
+handleData.then((data) => {
+	let listItems = createItemTemplate(data);
+	console.log(listItems);
+	let item = document.getElementById(DOMElements.itemContainer);
+	item.innerHTML = listItems;
+});
+
+function createItemTemplate(data) {
+	const item = data.reduce((result, item) => {
+		let template = `
+		<img src="${item.imageHref}">
+		<h2>${item.playlistName}</h2>
+		`;
+
+		result += template;
+		return result;
+	});
+	return item;
+}
