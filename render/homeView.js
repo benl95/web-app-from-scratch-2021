cae1ce3d5980a20ed2735218fb1470f2d529029d
benl95 <beno_1995@hotmail.com>
2021-02-24 13:07:41 +0100

    refactor: routing and rendering

--- render/homeView.js
+++ render/homeView.js
@@ -1,16 +1,24 @@
+import { handleData } from '../data/handleData.js';
+
 const DOMElements = {
 	listContainer: 'list-container',
 };
 
-export const renderHome = (playlistData) => {
-	const playlistItems = createItemTemplate(playlistData);
-	const listContainer = document.getElementById(DOMElements.listContainer);
-	listContainer.innerHTML = playlistItems;
+export const renderHome = () => {
+	handleData.then((playlistData) => {
+		renderItems(playlistData);
+	});
 };
+
+function renderItems(data) {
+	const listContainer = document.getElementById(DOMElements.listContainer);
+	const playlistItems = createItemTemplate(data);
+	listContainer.innerHTML = playlistItems;
+}
 
 function createItemTemplate(data) {
 	const playlistItems = data.reduce((item, key) => {
-		let template = `
+		const template = `
 		<div id="item-container">
 			<img src="${key.img}" alt="Playlist image">
 			<h2>${key.playlistName}</h2>
--- router/routeHandler.js
+++ router/routeHandler.js
@@ -1,12 +1,7 @@
-import { handleData } from '../data/handleData.js';
 import { renderHome } from '../render/homeView.js';
 
 export const Router = () => {
 	routie({
-		'': function () {
-			handleData.then((playlistData) => {
-				renderHome(playlistData);
-			});
-		},
+		'/': renderHome(),
 	});
 };
