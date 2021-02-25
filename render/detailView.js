8828ec8e98f9e8e229334162091bb613e15f0fca
benl95 <beno_1995@hotmail.com>
2021-02-25 19:26:59 +0100

    refactor: split code up into functions

--- render/detailView.js
+++ render/detailView.js
@@ -5,30 +5,44 @@ import { fetchData } from '../data/handleData.js';
 import { convertToJSON } from '../data/handleData.js';
 
 export const renderDetail = () => {
-	fetchPlaylist.then((playlistData) => {
-		Array.from(document.getElementsByClassName('item')).forEach((item) => {
-			item.addEventListener('click', () => {
-				const attrValue = Array.from(item.getAttribute('data-index'));
-				const id = attrValue.join('');
-				const endpoint = [
-					`https://api.spotify.com/v1/playlists/${id}/tracks`,
-				];
-				fetchData(endpoint, options)
-					.then(convertToJSON)
-					.then((data) => {
-						const tracks = data[0].items;
-						const filteredTracks = filterData(tracks, 'track', 'name');
-						renderItems(filteredTracks);
-						hide();
-					});
-			});
-		});
+	fetchPlaylist.then(() => {
+		render();
 	});
 };
 
-function hide() {
-	const listContainer = document.getElementById('list-container');
-	listContainer.setAttribute('class', 'toggle');
+function render() {
+	const items = document.getElementsByClassName('item');
+	const itemsToArray = Array.from(items);
+	getDataIndexElement(itemsToArray);
+}
+
+function getDataIndexElement(array) {
+	array.forEach((item) => {
+		item.addEventListener('click', () => {
+			const dataIndex = Array.from(item.getAttribute('data-index'));
+			console.log(dataIndex);
+			const id = dataIndex.join('');
+			console.log(id);
+			const tracksEndpoint = [
+				`https://api.spotify.com/v1/playlists/${id}/tracks`,
+			];
+
+			fetchData(tracksEndpoint, options)
+				.then(convertToJSON)
+				.then((data) => {
+					console.log(data);
+					const filteredTracks = filterTracks(data);
+					renderItems(filteredTracks);
+					hide();
+				});
+		});
+	});
+}
+
+function filterTracks(data) {
+	const tracks = data[0].items;
+	const filteredTracks = filterData(tracks, 'track', 'name');
+	return filteredTracks;
 }
 
 function createTracksTemplate(data) {
@@ -46,4 +60,9 @@ function renderItems(data) {
 	const tracksContainer = document.getElementById('tracks-list');
 	const trackItems = createTracksTemplate(data);
 	tracksContainer.innerHTML = trackItems;
+}
+
+function hide() {
+	const listContainer = document.getElementById('list-container');
+	listContainer.setAttribute('class', 'toggle');
 }
