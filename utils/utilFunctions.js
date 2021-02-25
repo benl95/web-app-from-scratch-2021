f6f308952390deff0929422cc590c8d4f6b017ee
benl95 <beno_1995@hotmail.com>
2021-02-25 14:16:38 +0100

    refactor filterData function

--- utils/utilFunctions.js
+++ utils/utilFunctions.js
@@ -1,4 +1,4 @@
 // Filter data stored in array based on the key that is passed
-export function filterData(array, key) {
-	return array.map((data) => data[key]);
+export function filterData(array, column, key) {
+	return array.map((data) => data[column][key]);
 }
