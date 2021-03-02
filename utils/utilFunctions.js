594b6e0bdf19a1b1d2d5923b4bb384b4dcab23c5
benl95 <beno_1995@hotmail.com>
2021-03-02 18:08:05 +0100

    remove filter function

--- utils/utilFunctions.js
+++ utils/utilFunctions.js
@@ -1,8 +1,3 @@
-// Filter data stored in array based on the key that is passed
-export function filterData(array, column, key) {
-	return array.map((data) => data[column][key]);
-}
-
 // https://stackoverflow.com/questions/21294302/converting-milliseconds-to-minutes-and-seconds-with-javascript#:~:text=var%20ms%20%3D%20298999%3B%20var%20min,min%2B'%3A'%2Bsec)%3B
 export function millisToMinutesAndSeconds(millis) {
 	var minutes = Math.floor(millis / 60000);
