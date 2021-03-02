bd639c0f12e4e650931b18b3afc3a3615ed4201b
benl95 <beno_1995@hotmail.com>
2021-03-02 18:44:22 +0100

    push config

---
+++ config.js
@@ -0,0 +1,3 @@
+export const playlistEndpoint = ['https://api.spotify.com/v1/me/playlists'];
+export const clientID = '5470641b38f34b889f84c88912b013a0';
+export const redirectURI = 'http://127.0.0.1:5500/index.html';
